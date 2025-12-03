9eb8ed066860063827ebf786d732a13dbc117ccc
quyen-dx <daoxuanquyen333@gmail.com>
2025-12-03 15:42:08 +0700

    ahihi

--- my-react-app/src/cpn/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center py-5 bg-gray-300 mt-10 text-sm text-gray-600">
-      © {new Date().getFullYear()} Tour Du Lịch. All rights reserved.
-    </footer>
-  );
-}
--- my-react-app/src/cpn/Title.jsx
+++
@@ -1,7 +0,0 @@
-export default function Title(props) {
-    return (
-        <div className="w-full  p-5 text-center">
-            <h1 className="text-red-600 font-bold text-[300%] text-center border-b-2 border-red-600 inline-block">{props.title}</h1>
-        </div>
-    )
-}
