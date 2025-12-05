dee4f7dd4e51595ec267edfb455c908236f4c43c
quyen-dx <daoxuanquyen333@gmail.com>
2025-12-05 15:46:03 +0700

    ahihi

--- WEB501-ECMA-Base-master/src/pages/Add.jsx
+++ WEB501-ECMA-Base-master/src/pages/Add.jsx
@@ -112,7 +112,7 @@ function Add() {
             className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
         </div>
-
+        
         <button
           type="submit"
           className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition"
